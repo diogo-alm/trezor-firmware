6a1be09ea982f3f56486208ee4e6b793d50c734e
diogo-alm <diogoalmmm@protonmail.com>
2020-05-24 18:23:57 +0200

    added AVA signing command to trezorctl

--- python/src/trezorlib/ava.py
+++ python/src/trezorlib/ava.py
@@ -7,3 +7,51 @@ def get_address(
     client, n,
 ):
     return client.call(messages.GetAVAAddress(address_n=n,))
+
+
+@expect(messages.AVASignedTx, field="tx")
+def sign_tx(
+    client,
+    network_id: int,
+    blockchain_id: bytes,
+    asset_id: bytes,
+    address_n: list,
+    transaction_id: bytes,
+    utxo_index: int,
+    utxo_amount: int,
+    to_address: str,
+    amount: int,
+    fxid: int,
+):
+
+    outputs = [
+        messages.AVAOutput(
+            asset=asset_id,
+            fxid=fxid,
+            amount=amount,
+            locktime=0,
+            threshold=1,
+            address=to_address,
+        )
+    ]
+
+    inputs = [
+        messages.AVAInput(
+            txid=transaction_id,
+            index=utxo_index,
+            asset=asset_id,
+            fxid=fxid,
+            amount=amount,
+        )
+    ]
+
+    return client.call(
+        messages.AVASignTx(
+            network=network_id,
+            blockchain_id=blockchain_id,
+            asset=asset_id,
+            address_n=address_n,
+            outputs=outputs,
+            inputs=inputs,
+        )
+    )
--- python/src/trezorlib/cli/ava.py
+++ python/src/trezorlib/cli/ava.py
@@ -1,3 +1,5 @@
+from hashlib import sha256
+
 import click
 
 from .. import ava, tools
@@ -21,3 +23,72 @@ def get_address(client, address):
     """Get address for specified path."""
     address_n = tools.parse_path(address)
     return ava.get_address(client, address_n)
+
+
+@cli.command()
+@click.option(
+    "-i", "--network-id", type=int, default=2, help="Network ID (replay protection)"
+)
+@click.option("-b", "--blockchain-id", required=True, help="Blockchain ID")
+@click.option("-a", "--asset-id", required=True, help="Asset ID")
+@click.option("-n", "--address", required=True, help="BIP-32 path")
+@click.option("-t", "--transaction-id", required=True, help="Transaction ID")
+@click.option("-u", "--utxo-index", required=True, help="UTXO index")
+@click.option("-v", "--utxo-amount", required=True, help="UTXO index")
+@click.argument("to_address")
+@click.argument("amount")
+@with_client
+def sign_tx(
+    client,
+    network_id,
+    blockchain_id,
+    asset_id,
+    address,
+    transaction_id,
+    utxo_index,
+    utxo_amount,
+    to_address,
+    amount,
+):
+    """Sign simple 1 output to 1 input transaction."""
+    address_n = tools.parse_path(address)
+    fxid = 0  # ava docs
+
+    def b58decode(v):
+        return tools.b58decode(v)[:-4]
+
+    blockchain_id = b58decode(blockchain_id)
+    asset_id = b58decode(asset_id)
+    transaction_id = b58decode(transaction_id)
+
+    tx = ava.sign_tx(
+        client,
+        network_id,
+        blockchain_id,
+        asset_id,
+        address_n,
+        transaction_id,
+        int(utxo_index),
+        int(utxo_amount),
+        to_address,
+        int(amount),
+        fxid,
+    )
+
+    checksum = sha256(tx).digest()
+    checksum = checksum[len(checksum) - 4 :]
+
+    tx = tools.b58encode(tx + checksum)
+    return tx
+
+
+# trezorctl ava sign-tx \
+#     -i 2 \
+#     -b 4ktRjsAKxgMr2aEzv9SWmrU7Xk5FniHUrVCX4P1TZSfTLZWFM \
+#     -a 21d7KVtPrubc5fHr6CGNcgbUb4seUjmZKr35ZX7BZb5iP8pXWA \
+#     -n "m/44'/909'/1/1" \
+#     -t 2DPGM3n7ZHumoPYoBP8LPi8Y5jqnVVU2adR83HHUqh56S8B82V \
+#     -u 0 \
+#     -v 20000 \
+#     X-MzyZFoD8pvAaNf5LpqWVnGZNb4NmsR3sj \
+#     19000
